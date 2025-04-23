fff426b4181769e1fc004c0f0f563398f2c0621c
MateoTorresD <mateoch632@gmail.com>
2025-04-22 21:16:42 -0500

    Update Cola.java

--- src/Cola.java
+++ src/Cola.java
@@ -5,18 +5,14 @@ import java.util.Queue;
 public class Cola {
     private Queue<Integer> queue = new LinkedList<>();
 
-    /**
-     * Encola un valor y actualiza el JTextArea.
-     */
+    
     public void encolar(int valor, JTextArea area) {
         queue.add(valor);
         area.append("Encolado: " + valor + "\n");
         area.append("Contenido cola: " + queue + "\n\n");
     }
 
-    /**
-     * Desencola un valor (si existe) y actualiza el JTextArea.
-     */
+    
     public void desencolar(JTextArea area) {
         if (queue.isEmpty()) {
             area.append("La cola está vacía.\n\n");
